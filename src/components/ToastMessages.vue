21e911a8faa7b3af4b29ef23f27e7b5f5b158c2d
KimberlyIn <51989121+KimberlyIn@users.noreply.github.com>
2021-07-21 17:41:27 +0800

    0721

--- src/components/ToastMessages.vue
+++ src/components/ToastMessages.vue
@@ -1,25 +1,27 @@
 <template>
-  <h3>這裡是 ToastMessages</h3>
-  <div 
-    class="toast-container position-absolute pe-3 top-0 end-0" 
-    style="z-index: 1500">
-    <div 
+  <div
+    class="toast-container position-absolute pe-3 top-0 end-0"
+    style="z-index: 1500"
+  >
+    <div
       v-for="(msg, key) in messages"
-      :key="key" 
+      :key="key"
       class="toast show"
-      :class="`toast${key}`" 
-      role="alert">
+      :class="`toast${key}`"
+      role="alert"
+    >
       <div class="toast-header">
-        <span 
-          :class="`bg-${msg.style}`" 
-          class="p-2 rounded me-2 d-inline-block">
-        </span>
+        <span
+          :class="`bg-${msg.style}`"
+          class="p-2 rounded me-2 d-inline-block"
+        ></span>
         <strong class="me-auto">{{ msg.title }}</strong>
-        <button 
-          type="button" 
-          class="btn-close" 
-          @click="clearToast(key)" 
-          aria-label="close"></button>
+        <button
+          type="button"
+          class="btn-close"
+          @click="clearToast(key)"
+          aria-label="Close"
+        ></button>
       </div>
       <div class="toast-body" v-if="msg.content">
         {{ msg.content }}
@@ -35,24 +37,19 @@ export default {
       messages: [],
     };
   },
-  // inject 注入 emitter 方法
   inject: ['emitter'],
   methods: {
-    // ?
     toastShow() {
       setTimeout(() => {
         this.messages.shift();
       }, 6000);
     },
-    // 清吐司
     clearToast(index) {
       this.messages.splice(index, 1);
     },
   },
   mounted() {
-    // 在這個地方製作監聽 使用 emitter.on 接收資料
     this.emitter.on('push-message', (message) => {
-      // 定義出 message 的結構並代入
       const { style = 'success', title, content } = message;
       this.messages.push({ style, title, content });
       this.toastShow();
